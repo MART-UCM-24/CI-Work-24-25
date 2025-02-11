e5e72033ff33ed4a55a4ea00817a1b646f35ebe9
martinrteran <martreigadas@gmail.com>
2025-02-11 21:45:56 +0100

    Submit

--- Network/DDPG.py
+++ Network/DDPG.py
@@ -6,15 +6,15 @@ from torch import optim
 import torch.nn as nn
 
 class DDPG:
-    def __init__(self, state_dim, action_dim, max_action,device):
+    def __init__(self, state_dim, action_dim, max_action, device):
         self.device = device
-        self.actor = Actor(state_dim, action_dim, max_action,device)#.to(device)
-        self.actor_target = Actor(state_dim, action_dim, max_action,device)#.to(device)
+        self.actor = Actor(state_dim, action_dim, max_action, device)
+        self.actor_target = Actor(state_dim, action_dim, max_action, device)
         self.actor_target.load_state_dict(self.actor.state_dict())
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=1e-4)
 
-        self.critic = Critic(state_dim, action_dim,device)#.to(device)
-        self.critic_target = Critic(state_dim, action_dim,device)#.to(device)
+        self.critic = Critic(state_dim, action_dim, device)
+        self.critic_target = Critic(state_dim, action_dim, device)
         self.critic_target.load_state_dict(self.critic.state_dict())
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=1e-3)
 
@@ -26,6 +26,13 @@ class DDPG:
 
     def train(self, replay_buffer, batch_size=64, discount=0.99, tau=0.005):
         state, action, next_state, reward, not_done = replay_buffer.sample(batch_size)
+
+        # Move tensors to the correct device
+        state = state.to(self.device)
+        action = action.to(self.device)
+        next_state = next_state.to(self.device)
+        reward = reward.to(self.device)
+        not_done = not_done.to(self.device)
 
         # Compute the target Q value
         target_Q = self.critic_target(next_state, self.actor_target(next_state))
@@ -56,3 +63,4 @@ class DDPG:
 
         for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
             target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
